c6f7b4667de078796c16b7415704189322e19f9f
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-24 21:24:14 +0600

    Add base template and bootstrap

--- tothastu_project/blog/views.py
+++ tothastu_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Farhan Fuad',
+        'title': 'Post 1 Title',
+        'text': 'Post 1 Description',
+        'post_date': '24-04-2021'
+    },
+    {
+        'author': 'Abir',
+        'title': 'Post 2 Title',
+        'text': 'Post 2 Description',
+        'post_date': '23-03-2020'
+    },
+]
+
 def home(request):
-    return HttpResponse("<h1>Home Page!</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>About Page!</h1>")
+    return render(request, 'blog/about.html', {'title': 'About Page'})
