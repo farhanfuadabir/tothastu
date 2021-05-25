4504d766d52e001edc1bfd922e01b423fb7dd728
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-25 20:31:26 +0600

    Add user profile template and routes

--- tothastu_project/tothastu_project/settings.py
+++ tothastu_project/tothastu_project/settings.py
@@ -127,4 +127,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = 'blog-home'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- tothastu_project/tothastu_project/urls.py
+++ tothastu_project/tothastu_project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('login/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('signin/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('signout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
 ]
